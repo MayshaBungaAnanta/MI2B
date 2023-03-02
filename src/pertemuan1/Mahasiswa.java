19152ec180252ac26a3250726cb2baab4c8c82d8
MayshaBungaAnanta <114631760+MayshaBungaAnanta@users.noreply.github.com>
2023-03-02 12:02:41 +0700

    [NF] Mahasiswa dan Pendaftaran

---
+++ src/pertemuan1/Mahasiswa.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan1;
+
+/**
+ *
+ * @author hp
+ */
+public class Mahasiswa {
+    String nama,nobp,kelas,prodi,jurusan;
+    
+    public static void main(String[] args){
+        Mahasiswa mhs = new Mahasiswa();
+        mhs.nama ="bunga";
+        mhs.nobp ="2101092031";
+        mhs.kelas ="mi2b";
+        mhs.prodi ="MI";
+        mhs.jurusan ="TI";
+        
+        System.out.println("Nama : " +mhs.nama+ "\n"
+                + "No BP : "+mhs.nobp+ "\n"
+                      + "Jurusan : "+mhs.jurusan+ "\n" 
+                            + "Prodi : "+mhs.prodi+ "\n" 
+                                + "Kelas : "+mhs.kelas+ "\n");
+        
+        
+        Mahasiswa mhs2 = new Mahasiswa();
+        mhs2.nama ="tiara";
+        mhs2.nobp ="2101092039";
+        mhs2.kelas ="mi2b";
+        mhs2.prodi ="MI";
+        mhs2.jurusan ="TI";
+        
+        System.out.println("Nama : " +mhs2.nama+ "\n"
+                + "No BP : "+mhs2.nobp+ "\n"
+                      + "Jurusan : "+mhs2.jurusan+ "\n" 
+                            + "Prodi : "+mhs2.prodi+ "\n" 
+                                + "Kelas : "+mhs2.kelas);
+    }
+}
