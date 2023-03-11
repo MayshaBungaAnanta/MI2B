7d072e60aab960915e2233aba9151f6b9e332c25
MayshaBungaAnanta <114631760+MayshaBungaAnanta@users.noreply.github.com>
2023-03-11 11:45:11 +0700

    [NF] Pertemuan 2

---
+++ src/pertemuan2/KelasKu.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan2;
+
+/**
+ *
+ * @author hp
+ */
+public class KelasKu {
+    
+   public static void main(String[] args) {
+       person objKu = new person();
+       objKu.name = "Bunga";
+       System.out.println("Nama : "+objKu.getName());
+   }
+}
